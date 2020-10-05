4a63bff2e5b213973408898603010459604edc04
darkpanda08 <vineetranjan65@gmail.com>
2020-10-05 16:36:50 +0530

    Authentication added

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2d4fc582abd6ceeb0c8045f58f06c882'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view='login'
+login_manager.login_message_category='info'
 
 from flaskblog import routes
